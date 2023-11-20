aa406fe4099f970f94bb490d79ff630d7403445f
yahya bnoun <yahyaiko22@gmail.com>
2023-11-20 21:29:45 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,130 @@
+// import java.math.BigDecimal;
+
+// import java.lang.reflect.Array;
+// import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // short b = 127;
+        // System.out.println("value of b is : "+ b);
+        // b++;
+        // System.out.println("value of b is : "+ Short.MAX_VALUE);
+        // System.out.println("value of b is : "+ Short.MIN_VALUE);
+
+        // -------------------------------- BigDecimal ------------------------------------------
+
+        // Double valueOf = .15;
+        // String TextOfValue = Double.toString(valueOf);
+
+        // BigDecimal CreateBigTotalValueOf = new BigDecimal(TextOfValue);
+        // BigDecimal TotalValueOf = CreateBigTotalValueOf.add(CreateBigTotalValueOf.add(CreateBigTotalValueOf));
+
+        // System.out.println("value of b is : " + TextOfValue);
+        // System.out.println("value of b is : " + TotalValueOf);
+
+        // -------------------------------- casting ------------------------------------------
+
+        // long Valuerx = 1024;
+        // System.out.println("long value  is : " +Valuerx);
+
+
+        // int Valuerxint = (int) Valuerx;
+        // System.out.println("int value  is : " +Valuerxint);
+
+        // short Valuerxshort = (short) Valuerx;
+        // System.out.println("short value  is : " +Valuerxshort);
+
+        // byte Valuerxbyte = (byte) Valuerx;
+        // System.out.println("byte value is : " +Valuerxbyte);
+
+
+        // --------------------------------  Booleans  ------------------------------------------  
+        
+        
+        // boolean Valuerx = false;
+
+        // System.out.println("boolean value is : " + Valuerx);
+
+
+        // String textboolean = "false";
+        // boolean convertToBoolean = Boolean.parseBoolean(textboolean) ;
+
+        // System.out.println("boolean value is : " + !convertToBoolean);
+
+
+
+        // --------------------------------  Characters   ------------------------------------------  
+
+        // char b = 'b';
+        // char c = 'c';
+        // char unicode_at = '\u0040';
+        // char unicode_dollar = '\u0024';
+
+
+        // System.out.println("char value toupper is : " + Character.toUpperCase(b));
+        // System.out.println("char value toupper is : " + Character.toUpperCase(c));
+        
+        // System.out.println("char value is : " + b);
+        // System.out.println("char value is : " + c);
+
+        // System.out.println("char value is : " + unicode_at);
+        // System.out.println("char value is : " + unicode_dollar);
+        
+        
+
+        // --------------------------------  Arrays  ------------------------------------------ 
+
+        // int arr[]  = {1,4,5,12};
+
+        
+        // System.out.println("length of array is : " + arr.length);
+        // System.out.println("length of array is : " + arr[2]);
+
+
+        // --------------------------------  Arrays sort()  ------------------------------------------ 
+        
+        // int arr[]  = {1,4,5,12};
+        
+        // Arrays.sort(arr);
+
+        // System.out.println("index 0 array is : " + arr[0]);
+        // System.out.println("index 1 array is : " + arr[1]);
+        // System.out.println("index 2 array is : " + arr[2]);
+        // System.out.println("index 3 array is : " + arr[3]);
+
+
+
+
+        // --------------------------------  Arraycopy()  ------------------------------------------ 
+
+
+
+        // int oreginal[]  = {1,4,5,12};
+        // int copy[]  = {1,4,5,12};
+        
+        // System.arraycopy(oreginal, 0, copy, 0, 0);;
+
+        // System.out.println("index 0 array is : " + copy[0]);
+        // System.out.println("index 1 array is : " + copy[1]);
+        // System.out.println("index 2 array is : " + copy[2]);
+        // System.out.println("index 3 array is : " + copy[3]);
+
+
+        // --------------------------------  Do while  ------------------------------------------ 
+
+        int num =0 , qeustion = 0;
+        do {
+            System.out.println("number of qeustion : " + num);
+            num++;
+        } while (num<3 && qeustion==1);
+
+
+        // --------------------------------  Do while  ------------------------------------------ 
+
+
+
+
+
+    }
+}
